066a3add8de971022c20907a26622847ae5e3bfe
Jesse H. Willett <jhw@prosperworks.com>
2016-08-02 15:46:06 -0700

    Cleanup

--- FOO.rb
+++
@@ -1,2 +0,0 @@
-
-DatabaseReplicationUtil.use_replica { Correspondence.connection.exec_query("SELECT id,to_hex(id),md5(to_hex(id)),concat(md5(to_hex(id)),'/',to_hex(id)) FROM correspondences WHERE concat(md5(to_hex(id)),'/',to_hex(id)) <= '001' ORDER BY md5(to_hex(id)) ASC LIMIT 5") }
