91d3f0b19d22fd2fce8ee2376740f3ccfc69c2ca
Jesse H. Willett <jhw@prosperworks.com>
2016-11-18 13:53:17 -0800

    Stuff

---
+++ bin/get-brew-stuff.sh
@@ -0,0 +1,5 @@
+# stuff I value or need from brew on my Macs
+
+brew install coreutils moreutils postgres pgbouncer homebrew/versions/redis28 twemproxy citus imagemagick
+brew install homebrew/versions/elasticsearch24 
+brew install watch
