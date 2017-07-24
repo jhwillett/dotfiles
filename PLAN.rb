3455b6c21117d32259e2953e3b1dfb3e9ee1ed0d
jhw-at-prosperworks-com <jhw@prosperworks.com>
2017-07-24 11:53:50 -0700

    Update PLAN.rb.

--- PLAN.rb
+++ PLAN.rb
@@ -1,33 +1,22 @@
 
-heroku config:set BATCHER_ES_INDEXER_INSTANCES=AWS_ES_05,AWS_ES_10,AWS_ES_12,AWS_ES_14,AWS_ES_16 BATCHER_ES_REINDEX_INSTANCES=AWS_ES_14,AWS_ES_16 BATCHER_ES_REREINDEX_INSTANCES=AWS_ES_14,AWS_ES_16 BATCHER_ES_STATS_INSTANCES=AWS_ES_05,AWS_ES_10,AWS_ES_12,AWS_ES_14,AWS_ES_16 BATCHER_ES_VERIFICATION_INSTANCES=AWS_ES_05,AWS_ES_10,AWS_ES_12,AWS_ES_14,AWS_ES_16 BATCHER_ES_WRITE_INSTANCES=AWS_ES_05,AWS_ES_10,AWS_ES_12,AWS_ES_14,AWS_ES_16 --app ali-production
+heroku config:set --app ali-production BATCHER_ES_INDEXER_INSTANCES=AWS_ES_14,AWS_ES_16 BATCHER_ES_REINDEX_INSTANCES= BATCHER_ES_REREINDEX_INSTANCES= BATCHER_ES_STATS_INSTANCES=AWS_ES_14,AWS_ES_16 BATCHER_ES_VERIFICATION_INSTANCES=AWS_ES_14,AWS_ES_16 BATCHER_ES_WRITE_INSTANCES=AWS_ES_14,AWS_ES_16
 
-heroku config:unset AWS_ES_02_AWS_KEY AWS_ES_02_AWS_REGION AWS_ES_02_AWS_SECRET AWS_ES_02_URL AWS_ES_08_AWS_KEY AWS_ES_08_AWS_REGION AWS_ES_08_AWS_SECRET AWS_ES_08_INDEX_PER_MAPPING AWS_ES_08_URL AWS_ES_09_AWS_KEY AWS_ES_09_AWS_REGION AWS_ES_09_AWS_SECRET AWS_ES_09_PARTIAL_REINDEXING AWS_ES_09_URL AWS_ES_11_AWS_KEY AWS_ES_11_AWS_REGION AWS_ES_11_AWS_SECRET AWS_ES_11_PARTIAL_REINDEXING AWS_ES_11_URL AWS_ES_13_AWS_KEY AWS_ES_13_AWS_REGION AWS_ES_13_AWS_SECRET AWS_ES_13_PARTIAL_REINDEXING AWS_ES_13_URL AWS_ES_15_AWS_KEY AWS_ES_15_AWS_REGION AWS_ES_15_AWS_SECRET AWS_ES_15_PARTIAL_REINDEXING AWS_ES_15_URL --app ali-production
+heroku config:unset --app ali-production AWS_ES_05_AWS_KEY AWS_ES_05_AWS_REGION AWS_ES_05_AWS_SECRET AWS_ES_05_URL AWS_ES_10_AWS_KEY AWS_ES_10_AWS_REGION AWS_ES_10_AWS_SECRET AWS_ES_10_PARTIAL_REINDEXING AWS_ES_10_URL AWS_ES_12_AWS_KEY AWS_ES_12_AWS_REGION AWS_ES_12_AWS_SECRET AWS_ES_12_PARTIAL_REINDEXING AWS_ES_12_URL
 
 [
-  'AWS_ES_02',
-  'AWS_ES_08',
-  'AWS_ES_09',
-  'AWS_ES_11',
-  'AWS_ES_13',
-  'AWS_ES_15',
-].each do |instance|
-  [
-    'indexer_primary',
-    'indexer_bilge',
-    'deleter_primary',
-    'deleter_bilge',
-    'verifier',
-  ].each do |flavor|
+  'indexer_primary','indexer_bilge','deleter_primary','deleter_bilge','verifier'
+].each do |flavor|
+  [ 'AWS_ES_05','AWS_ES_10','AWS_ES_12' ].each do |instance|
     batcher_name = "es_#{instance}_#{flavor}"
+    ick          = Ick.new(Redis.current)
     50.times do |shard_id|
       ick_name   = Batchers::Batcher.sharded_queue_name(batcher_name,shard_id)
-      ick_stats  = Ick.ickstats(Redis.current,ick_name)
+      ick_stats  = ick.ickstats(ick_name)
       next if !ick_stats
       next if ick_stats['total_size'] <= 0
       puts "ick_name:  #{ick_name}"
-      #puts "ick_stats: #{ick_stats}"
       puts "total_size: #{ick_stats['total_size']}"
-      #Ick.ickdel(Redis.current,ick_name)
+      #ick.ickdel(ick_name)                # uncomment to del if sizes are sane
     end
   end
 end
