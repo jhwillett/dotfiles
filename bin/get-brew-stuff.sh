700a03a2b04ececd72157bd9f69ca95367183102
Jesse H. Willett <jhwjhw@gmail.com>
2021-12-10 10:04:47 -0800

    Cut now-unused things.

--- bin/get-brew-stuff.sh
+++
@@ -1,5 +0,0 @@
-# stuff I value or need from brew on my Macs
-
-brew install coreutils moreutils postgres pgbouncer homebrew/versions/redis28 twemproxy citus imagemagick
-brew install homebrew/versions/elasticsearch24 
-brew install watch
