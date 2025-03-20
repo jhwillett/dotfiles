66f6fe6ecf400cf887bb23adaac2cea4e6b91321
Jesse Willett <jhwjhw@roundingerrorgames.com>
2025-03-20 11:09:41 -0700

    Added emacs markdown-mode.

--- .emacs.el
+++ .emacs.el
@@ -359,3 +359,16 @@
 ;;
 (add-to-list 'load-path "~/.elisp/wgsl-mode")
 (load "wgsl-mode")
+
+;; markdown-mode https://jblevins.org/projects/markdown-mode/
+;;
+(add-to-list 'load-path "~/.elisp/markdown-mode-2.7")
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist
+             '("\\.\\(?:md\\|markdown\\|mkd\\|mdown\\|mkdn\\|mdwn\\)\\'" . markdown-mode))
+(autoload 'gfm-mode "markdown-mode"
+   "Major mode for editing GitHub Flavored Markdown files" t)
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+(with-eval-after-load 'markdown-mode
+  (define-key markdown-mode-map (kbd "C-c C-e") #'markdown-do))
